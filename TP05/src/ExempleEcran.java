e608cb49c06842b81bfe5d14ad41fdcb5f0fed6f
elhyanich <elhayanich@gmail.com>
2026-01-08 16:00:42 +0100

    exemple ecran 0701

--- TP05/src/ExempleEcran.java
+++ TP05/src/ExempleEcran.java
@@ -9,14 +9,23 @@ class ExempleEcran {
 
 	public static void main(String[] args) {
 		// Construire un écran
+		  Ecran e = new Ecran("ExempleEcran", 400, 250, 15);
+		
 
 		// Dessiner un point vert de coordonnées (1, 2)
 
+		e.dessinerPoint(1, 2, Color.Vert);
+		
+
 		// Dessiner un segment rouge d'extrémités (6, 2) et (11, 9)
 
+		e.dessinerLigne(6, 2, 11, 9, Color.Rouge);
+
 		// Dessiner un cercle jaune de centre (4, 3) et rayon 2.5
+		 e.dessinerCercle(4, 3, 2.5, Color.Jaune);
 
 		// Dessiner le texte "Premier dessin" en bleu à la position (1, -2)
+		e.dessinerTexte(1, -2, "Premier dessin", Color.Bleu);
 	}
 
 }
